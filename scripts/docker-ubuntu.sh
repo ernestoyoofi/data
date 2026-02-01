75bb6753b0bb71e6bca37fb1cdae1813f34a46b2
Ernestoyoofi <94425296+ernestoyoofi@users.noreply.github.com>
2026-02-01 18:19:37 +0700

    Update docker-ubuntu.sh

--- scripts/docker-ubuntu.sh
+++ scripts/docker-ubuntu.sh
@@ -1,9 +1,11 @@
+# Update
 sudo apt update
 sudo apt install ca-certificates curl
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
 
+# Add Source List
 sudo tee /etc/apt/sources.list.d/docker.sources <<EOF
 Types: deb
 URIs: https://download.docker.com/linux/ubuntu
@@ -12,10 +14,8 @@ Components: stable
 Signed-By: /etc/apt/keyrings/docker.asc
 EOF
 
+# Install & Testing
 sudo apt update
-
-sudo apt install docker-ce docker-ce-cli containerd.io
-
+sudo apt install docker-ce docker-ce-cli containerd.io -y
 sudo systemctl status docker
-
 sudo docker run hello-world
