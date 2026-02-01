46676edeb4653d83c36f7643fa3448fad8a727f6
Ernestoyoofi <94425296+ernestoyoofi@users.noreply.github.com>
2026-02-01 18:24:25 +0700

    fix: Enhance docker-ubuntu.sh for Docker installation

--- scripts/docker-ubuntu.sh
+++ scripts/docker-ubuntu.sh
@@ -1,11 +1,15 @@
-# Update
+#!/bin/bash
+
+# 1. Update & Install Dependencies
 sudo apt update
-sudo apt install ca-certificates curl
+sudo apt install -y ca-certificates curl
+
+# 2. Setup Keyring
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
 
-# Add Source List
+# 3. Add Source List
 sudo tee /etc/apt/sources.list.d/docker.sources <<EOF
 Types: deb
 URIs: https://download.docker.com/linux/ubuntu
@@ -14,8 +18,15 @@ Components: stable
 Signed-By: /etc/apt/keyrings/docker.asc
 EOF
 
-# Install & Testing
+# 4. Install Docker
 sudo apt update
-sudo apt install docker-ce docker-ce-cli containerd.io -y
-sudo systemctl status docker
-sudo docker run hello-world
+sudo DEBIAN_FRONTEND=noninteractive apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# 5. Testing & Verification
+echo "Checking..."
+sudo systemctl enable --now docker
+sudo docker run --rm hello-world
+
+echo "---"
+echo "Finish!"
+docker --version
